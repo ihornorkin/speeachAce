f581822253f3d2e1daa714dcf217cadcf8626b57
ihornorkin <ihornorkin@gmail.com>
2017-05-31 11:40:06 +0300

    Test commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -281,8 +281,8 @@ const mainModule = (function () {
 				]
 			});
 			cachedDOM.sliderNav.not('.slick-initialized').slick({
-				dots: false,
 				infinite: false,
+				dots: false,
 				speed: 300,
 				slidesToShow: 3,
 				slidesToScroll: 1,
